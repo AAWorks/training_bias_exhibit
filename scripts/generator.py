be3738310bc82cee035c37529e31ce25220339ee
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-04-11 16:04:43 -0500

    dataset generator

--- scripts/generator.py
+++ scripts/generator.py
@@ -1,50 +1,64 @@
 from random import randint, choice
 import pandas as pd
+import streamlit as st
+from scripts.classifier import AlgoUtils
 
-class Generator:
+class TestSet:
     """generate the unbiased and biased dataset based on user specficiations"""
 
-    data: pd.DataFrame
-    total_applicants: int
-    hires: int
-    def __init__(self, num_applicants, num_hires, larger_ratio, bias):
-        self.data = pd.DataFrame(columns=['Gender', 'Skill', 'Academics', 'Experience', 'Ambition'])
+    def __init__(self, num_applicants, num_hires, bias):
+        self.biased_data = pd.DataFrame(columns=['Gender', 'Skill', 'Academics', 'Experience', 'Ambition'])
+        self.unbiased_data = pd.DataFrame(columns=['Gender', 'Skill', 'Academics', 'Experience', 'Ambition'])
         self.total_applicants = num_applicants
         self.hires = num_hires
-        self.larger_ratio = larger_ratio
-        self.bias = bias
+        self.bias = bias.lower()
 
     def __get_stats(self, gender, a, b):
         stats = [gender] + [randint(a, b) for _ in range(4)] # 0 is male, 1 is female
         return stats
     
-    def __get_cv(self, ratios):
-        if ratios[1] == 'male':
-            if randint(1, 100) < ratios[0]:
-                cv = generate_stats(0, 4, 9)
+    def __get_cv(self):
+        if self.bias == "male":
+            if randint(1, 100) < 50:
+                cv = self.__get_stats(0, 2, 9)
             else:
-                cv = generate_stats(1, 2, 6)
+                cv = self.__get_stats(1, 2, 6)
         else:
-            if randint(1, 100) < ratios[0]:
-                cv = generate_stats(1, 4, 9)
+            if randint(1, 100) < 50:
+                cv = self.__get_stats(1, 2, 9)
             else:
-                cv = generate_stats(0, 2, 6)
+                cv = self.__get_stats(0, 2, 6)
         return cv
 
-    def biased_dataset(self, biased_data):
-        print('Creating the biased dataset...')
+    def biased_dataset(self):
+        biased_data = self.biased_data
+        progress_text = "Generating biased dataset..."
+        my_bar = st.progress(0, text=progress_text)
+        unit = self.total_applicants // 100
 
-        ratios = define_ratios()
-        for applicant in range(self.total_applicants):
-            cv = generate_cv(ratios)
+        for i in range(self.total_applicants):
+            cv = self.__get_cv()
             biased_data = biased_data.append({'Gender': cv[0], 'Skill': cv[1], 'Academics': cv[2], 'Experience': cv[3], 'Ambition': cv[4]}, ignore_index = True)
-        target=functions.label_accepted(biased_data, self.hires)
-        print('Done. \n')
-        return biased_data, [biased_data.values, target]
-  def unbiased_dataset(self, unbiased_data):
-    print('Creating unbiased dataset...')
-    for applicant in range(self.total_applicants):
-      unbiased_data = unbiased_data.append({'Gender': choice([0, 1]), 'Skill': randint(2, 9), 'Academics': randint(2,9), 'Experience': randint(2,9), 'Ambition': randint(2,9)}, ignore_index = True)
-    target=functions.label_accepted(unbiased_data, self.hires)
-    print('Done. \n')
-    return unbiased_data, [unbiased_data.values, target]
+            if i % unit == 0:
+                percent_complete = i / unit
+                my_bar.progress(percent_complete / 100, text=progress_text)
+
+        target = AlgoUtils.label_accepted(biased_data, self.hires)
+        my_bar.progress(1.0, text="Generated Biased Dataset :white_check_mark:")
+        return biased_data, (biased_data.values, target)
+
+    def unbiased_dataset(self):
+        unbiased_data = self.unbiased_data
+        progress_text = "Generating unbiased dataset..."
+        my_bar = st.progress(0, text=progress_text)
+        unit = self.total_applicants // 100
+
+        for i in range(self.total_applicants):
+            unbiased_data = unbiased_data.append({'Gender': choice([0, 1]), 'Skill': randint(2, 9), 'Academics': randint(2,9), 'Experience': randint(2,9), 'Ambition': randint(2,9)}, ignore_index = True)
+            if i % unit == 0:
+                percent_complete = i / unit
+                my_bar.progress(percent_complete / 100, text=progress_text)
+
+        target = AlgoUtils.label_accepted(unbiased_data, self.hires)
+        my_bar.progress(1.0, text="Generated Unbiased Dataset :white_check_mark:")
+        return unbiased_data, (unbiased_data.values, target)
